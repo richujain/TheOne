28df2f009c817abd21090c1bd96ca007f63578b3
Richu <richujain1995@gmail.com>
2021-06-04 16:41:11 -0400

    [Richu] Fragments added to HomeActivity.kt. Decided to keep sendFragment as the initial fragment. Working on send fragment now.

--- app/src/main/java/com/richujain/theone/HomeActivity.kt
+++ app/src/main/java/com/richujain/theone/HomeActivity.kt
@@ -4,17 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.richujain.theone.fragments.AccountFragment
+import com.richujain.theone.fragments.CloudFragment
+import com.richujain.theone.fragments.GalleryFragment
+import com.richujain.theone.fragments.SendFragment
 import nl.joery.animatedbottombar.AnimatedBottomBar
 import kotlin.properties.Delegates
 
 class HomeActivity : AppCompatActivity() {
+    private val sendFragment = SendFragment()
+    private val galleryFragment = GalleryFragment()
+    private val cloudFragment = CloudFragment()
+    private val accountFragment = AccountFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val bottom_bar = findViewById<AnimatedBottomBar
                 >(R.id.bottom_bar)
+        replaceFragment(sendFragment)
         bottom_bar.onTabSelected = {
-            Log.d("bottom_bar", "Selected tab: " + it.title)
+            when(it.title){
+                "Send" -> replaceFragment(sendFragment)
+                "Gallery" -> replaceFragment(galleryFragment)
+                "Cloud" -> replaceFragment(cloudFragment)
+                "Account" -> replaceFragment(accountFragment)
+            }
         }
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer,fragment)
+        transaction.commit()
+    }
+
 }
