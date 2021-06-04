963cc0e6925cdf72844dcc837370f45f17fdfb8d
Richu <richujain1995@gmail.com>
2021-06-04 14:42:40 -0400

    [Richu] Created required fragments.

--- app/src/main/java/com/richujain/theone/HomeActivity.kt
+++ app/src/main/java/com/richujain/theone/HomeActivity.kt
@@ -8,14 +8,13 @@ import nl.joery.animatedbottombar.AnimatedBottomBar
 import kotlin.properties.Delegates
 
 class HomeActivity : AppCompatActivity() {
-//    val bottom_bar = findViewById<AnimatedBottomBar
-//            >(R.id.bottom_bar)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-//        bottom_bar.onTabSelected = {
-//            Log.d("bottom_bar", "Selected tab: " + it.title)
-//        }
+        val bottom_bar = findViewById<AnimatedBottomBar
+                >(R.id.bottom_bar)
+        bottom_bar.onTabSelected = {
+            Log.d("bottom_bar", "Selected tab: " + it.title)
+        }
     }
 }
