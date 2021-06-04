572c48c909252c6ac71081c9454ebbf7b9f0f690
Richu <richujain1995@gmail.com>
2021-06-04 00:31:58 -0400

    [Richu] Created HomeActivity.kt and added bottom navigation menu to it. Working on LoginActivity.

---
+++ app/src/main/java/com/richujain/theone/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.richujain.theone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
