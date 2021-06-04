8442eb1363df0c90d6207dba8ff637911b8ac71e
Richu <richujain1995@gmail.com>
2021-06-04 13:45:57 -0400

    [Richu] Designed LoginActivity.kt and created a custom tooolbar for HomeActivity.kt. Also, changed the primary colors.

--- app/src/main/java/com/richujain/theone/HomeActivity.kt
+++ app/src/main/java/com/richujain/theone/HomeActivity.kt
@@ -2,10 +2,20 @@ package com.richujain.theone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import nl.joery.animatedbottombar.AnimatedBottomBar
+import kotlin.properties.Delegates
 
 class HomeActivity : AppCompatActivity() {
+//    val bottom_bar = findViewById<AnimatedBottomBar
+//            >(R.id.bottom_bar)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+//        bottom_bar.onTabSelected = {
+//            Log.d("bottom_bar", "Selected tab: " + it.title)
+//        }
     }
 }
--- app/src/main/java/com/richujain/theone/LoginActivity.kt
+++ app/src/main/java/com/richujain/theone/LoginActivity.kt
@@ -1,12 +1,37 @@
 package com.richujain.theone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val buttonLogin = findViewById<Button
+                >(R.id.buttonLogin)
+        buttonLogin.setOnClickListener(){
+            if(login()){
+                intent = Intent(this,HomeActivity::class.java)
+                startActivity(intent)
+            }
+            else
+                Toast.makeText(this@LoginActivity, "Incorrect Credentials!", Toast.LENGTH_SHORT).show()
+        }
 
     }
+
+    private fun login(): Boolean {
+        val editTextEmail = findViewById<EditText
+                >(R.id.editTextForEmail)
+        val editTextPassword = findViewById<EditText
+                >(R.id.editTextForPassword)
+        return editTextEmail.text.trim().toString().equals("a") && editTextPassword.text.trim().toString().equals("a")
+    }
+
 }
